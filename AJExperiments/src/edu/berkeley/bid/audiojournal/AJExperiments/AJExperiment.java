8ce9b462d922addaed8076a28d7e5deb7ec3cb4e
Javier Rosa <javirosa1912@gmail.com>
2011-03-20 00:02:01 -0700

    Initialize

---
+++ AJExperiments/src/edu/berkeley/bid/audiojournal/AJExperiments/AJExperiment.java
@@ -0,0 +1,13 @@
+package edu.berkeley.bid.audiojournal.AJExperiments;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AJExperiment extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
